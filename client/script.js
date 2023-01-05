a1d5294065ce1e4b3d3e76c3f108f11dade3d1a5
111vandit111 <vanditkala@gmail.com>
2023-01-05 22:06:36 +0530

    updated server link

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handelSubmit = async (e) => {
 
  console.log(data.get("prompt"))
    
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://black-gpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
